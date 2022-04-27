5a793f172f9e627c1bf485cc0fd5ac47cd7c5429
Danieli-Bauer <danielibauer60@gmail.com>
2022-04-27 19:30:51 -0300

    Inclusão do desafio 2

--- aula01/07_desafio_2.py
+++ aula01/07_desafio_2.py
@@ -1,8 +1,13 @@
 # 1. Pergunte ao usuário em qual cidade e o estado em que ele(a) mora
+cidade=input("Qual sua cidade?")
+estado=input("Qual seu estado?")
+print(cidade.upper())
 
 # 2. Imprima uma mensagem diga a cidade em que o usuário mora. 
 #    O nome da cidade deve estar todo em letras maiúsculas.
+estado_maiusculo = estado.upper()
 
 # 3. Imprima uma mensagem diga o estado em que o usuário mora. 
 #    O nome do estado deve estar todo em letras maiúsculas.
 #    Utilize uma forma de formatação de string diferente da que foi usada no item 2.
+print(estado_maiusculo)
